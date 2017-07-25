6c0844138c13ed586102c65b9d64692191a76abc
tbscanlon <thomas.scanlon@outlook.com>
2017-07-25 18:46:21 +0100

    Working implementation of API model

--- spec/unit/apiSpec.js
+++ spec/unit/apiSpec.js
@@ -4,8 +4,8 @@
   window.addEventListener("load", function(){
     api = new Api();
 
-    api.get( "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics/blog/2014/feb/17/alex-salmond-speech-first-minister-scottish-independence-eu-currency-live?show-fields=body", function () {
-      assert.isEqual(api.last().response.content.webTitle,
+    api.get( "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/politics/blog/2014/feb/17/alex-salmond-speech-first-minister-scottish-independence-eu-currency-live?show-fields=body", function (results) {
+      assert.isEqual(results.response.content.webTitle,
                     "Alex Salmond speech – first minister hits back over Scottish independence",
                     "Requests can be made to external APIs");
     });
